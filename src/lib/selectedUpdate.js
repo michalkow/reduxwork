cf1dd89e6c0bd5c61f23e8c4c2f10823cf45f893
Michał Kowalkowski <kowalkowski.michal@gmail.com>
2017-07-10 19:40:02 +0200

    fix

--- src/lib/selectedUpdate.js
+++ src/lib/selectedUpdate.js
@@ -1,6 +1,6 @@
 export default function selectedUpdate(config, state, items) {
 	var update = {};
 	if(state.selected && state.selected[config.keyName]) 
-		update.selected = _.find(action.data, (item) => item[config.keyName] == state.selected[config.keyName]);
+		update.selected = _.find(items, (item) => item[config.keyName] == state.selected[config.keyName]);
 	return update;
 }
