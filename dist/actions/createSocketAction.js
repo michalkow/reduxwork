de6add843ef8feb153735acc3e30ea01d4d11fba
Michał Kowalkowski <kowalkowski.michal@gmail.com>
2019-12-19 15:28:39 +0100

    untrack

--- dist/actions/createSocketAction.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = createSocketAction;var _buildAction = _interopRequireDefault(require("./buildAction"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-
-function createSocketAction() {var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};var name = arguments.length > 1 ? arguments[1] : undefined;
-  config.type = 'socket';
-  return function (data, cb) {
-    return (0, _buildAction.default)(config, null, name, data, cb);
-  };
-}
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NvdXJjZS9hY3Rpb25zL2NyZWF0ZVNvY2tldEFjdGlvbi5qcyJdLCJuYW1lcyI6WyJjcmVhdGVTb2NrZXRBY3Rpb24iLCJjb25maWciLCJuYW1lIiwidHlwZSIsImRhdGEiLCJjYiJdLCJtYXBwaW5ncyI6ImdIQUFBLG9FOztBQUVlLFNBQVNBLGtCQUFULEdBQStDLEtBQW5CQyxNQUFtQix1RUFBVixFQUFVLEtBQU5DLElBQU07QUFDNURELEVBQUFBLE1BQU0sQ0FBQ0UsSUFBUCxHQUFjLFFBQWQ7QUFDQSxTQUFPLFVBQVVDLElBQVYsRUFBZ0JDLEVBQWhCLEVBQW9CO0FBQ3pCLFdBQU8sMEJBQVlKLE1BQVosRUFBb0IsSUFBcEIsRUFBMEJDLElBQTFCLEVBQWdDRSxJQUFoQyxFQUFzQ0MsRUFBdEMsQ0FBUDtBQUNELEdBRkQ7QUFHRCIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBidWlsZEFjdGlvbiBmcm9tICcuL2J1aWxkQWN0aW9uJztcclxuXHJcbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIGNyZWF0ZVNvY2tldEFjdGlvbihjb25maWcgPSB7fSwgbmFtZSkge1xyXG4gIGNvbmZpZy50eXBlID0gJ3NvY2tldCc7XHJcbiAgcmV0dXJuIGZ1bmN0aW9uIChkYXRhLCBjYikge1xyXG4gICAgcmV0dXJuIGJ1aWxkQWN0aW9uKGNvbmZpZywgbnVsbCwgbmFtZSwgZGF0YSwgY2IpO1xyXG4gIH07XHJcbn0iXX0=
--- dist/lib/createInitialState.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var initialState = {
-  actionCache: {},
-  entitieStatus: {},
-  actionErrors: {},
-  lastAction: null };var _default =
-
-
-function _default() {return initialState;};exports.default = _default;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NvdXJjZS9saWIvY3JlYXRlSW5pdGlhbFN0YXRlLmpzIl0sIm5hbWVzIjpbImluaXRpYWxTdGF0ZSIsImFjdGlvbkNhY2hlIiwiZW50aXRpZVN0YXR1cyIsImFjdGlvbkVycm9ycyIsImxhc3RBY3Rpb24iXSwibWFwcGluZ3MiOiJvR0FBQSxJQUFNQSxZQUFZLEdBQUc7QUFDbkJDLEVBQUFBLFdBQVcsRUFBRSxFQURNO0FBRW5CQyxFQUFBQSxhQUFhLEVBQUUsRUFGSTtBQUduQkMsRUFBQUEsWUFBWSxFQUFFLEVBSEs7QUFJbkJDLEVBQUFBLFVBQVUsRUFBRSxJQUpPLEVBQXJCLEM7OztBQU9lLDRCQUFNSixZQUFOLEUiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBpbml0aWFsU3RhdGUgPSB7XHJcbiAgYWN0aW9uQ2FjaGU6IHt9LFxyXG4gIGVudGl0aWVTdGF0dXM6IHt9LFxyXG4gIGFjdGlvbkVycm9yczoge30sXHJcbiAgbGFzdEFjdGlvbjogbnVsbFxyXG59O1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgKCkgPT4gaW5pdGlhbFN0YXRlOyJdfQ==
